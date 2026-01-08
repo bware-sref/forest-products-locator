c16c25529b862d70d6329723834640693db8ee5e
SREF <bware@sref.info>
2026-01-08 21:58:04 +0000

    Add pin for Warnell

--- resources/js/components/basic-map.tsx
+++ resources/js/components/basic-map.tsx
@@ -1,12 +1,21 @@
-import { Map, MapTileLayer } from "@/components/ui/map"
+import { Map, MapMarker, MapTileLayer } from "@/components/ui/map"
 import type { LatLngExpression } from "leaflet"
 
 export function BasicMap() {
     const WARNELL_COORDINATES = [33.9439, -83.3769] satisfies LatLngExpression
+    const PINS = [
+        {
+            name: "Warnell School of Forestry and Natural Resources",
+            coordinates: WARNELL_COORDINATES,
+        },
+    ];
 
     return (
         <Map center={WARNELL_COORDINATES}>
             <MapTileLayer />
+            {PINS.map((pin) => (
+                <MapMarker key={pin.name} position={pin.coordinates} />
+            ))}
         </Map>
     )
 }
