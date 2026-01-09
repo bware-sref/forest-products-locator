8bc57103ca51164d8bd09a95d11cc6884c02bad5
SREF <bware@sref.info>
2026-01-09 15:17:30 +0000

    Add map pin popup, locator button, and locator button action

--- resources/js/components/basic-map.tsx
+++ resources/js/components/basic-map.tsx
@@ -1,5 +1,17 @@
-import { Map, MapMarker, MapTileLayer } from "@/components/ui/map"
+// we have to add "use client" to be able to use callbacks with MapLocateControl
+// "use client" is also required to use WMS (or so I led myself to believe...)
+"use client"
+
+import { 
+    Map,
+    MapLocateControl,
+    MapMarker,
+    MapPopup,
+    MapTileLayer
+} from "@/components/ui/map"
 import type { LatLngExpression } from "leaflet"
+import { useState } from "react"
+import { toast } from "sonner"
 
 export function BasicMap() {
     const WARNELL_COORDINATES = [33.9439, -83.3769] satisfies LatLngExpression
@@ -9,13 +21,33 @@ export function BasicMap() {
             coordinates: WARNELL_COORDINATES,
         },
     ];
+    const [myCoordinates, setMyCoordinates] = useState<LatLngExpression | null>(
+        null
+    )
 
     return (
         <Map center={WARNELL_COORDINATES}>
             <MapTileLayer />
             {PINS.map((pin) => (
-                <MapMarker key={pin.name} position={pin.coordinates} />
+                <MapMarker key={pin.name} position={pin.coordinates}>
+                    <MapPopup className="w-56">{pin.name}</MapPopup>
+                </MapMarker>
             ))}
+            <MapLocateControl 
+                onLocationFound={(location) =>
+                    setMyCoordinates(location.latlng)
+                }
+                onLocationError={(error) => toast.error(error.message)}
+                watch
+            />
+            {myCoordinates && (
+                <MapPopup
+                    position={myCoordinates}
+                    offset={[0, -5]}
+                    className="w-56">
+                    {myCoordinates.toString()}
+                </MapPopup>
+            )}
         </Map>
     )
 }
