19774e899923dd716a0d25d9b41cfce417acef7e
Bob Ware <bware@sref.info>
2026-01-21 16:35:46 -0500

    add routes for top level pages

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,27 @@ use Laravel\Fortify\Features;
 
 Route::get('/', function () {
     return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
+        // 'canRegister' => Features::enabled(Features::registration()),
     ]);
 })->name('home');
+
+/**
+ * Add routes for all pages.
+ * mill-map
+ * mill-list
+ * about-us
+ */
+Route::get('/mill-map', function () {
+    return Inertia::render('mill-map', []);
+})->name('mill-map');
+Route::get('/mill-list', function () {
+    return Inertia::render('mill-list', []);
+})->name('mill-list');
+Route::get('/about-us', function () {
+    return Inertia::render('about-us', []);
+})->name('about-us');
+
+
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
