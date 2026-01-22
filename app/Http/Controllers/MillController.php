2ab4516d2a5b1cd07f2268afb34a6380993088a7
Bob Ware <bware@sref.info>
2026-01-22 12:26:26 -0500

    add Mill MVC files

---
+++ app/Http/Controllers/MillController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreMillRequest;
+use App\Http\Requests\UpdateMillRequest;
+use App\Models\Mill;
+
+class MillController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMillRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Mill $mill)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMillRequest $request, Mill $mill)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Mill $mill)
+    {
+        //
+    }
+}
