4117e41ccb6ad1b5011f467f9e5ff3b19021725d
Bob Ware <bware@sref.info>
2026-01-23 13:05:03 -0500

    add Laravel routes for existing site pages

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,24 @@ Route::get('/mill-list', function () {
 Route::get('/about-us', function () {
     return Inertia::render('about-us', []);
 })->name('about-us');
+Route::get('/state-resources', function () {
+    return Inertia::render('state-resources', []);
+})->name('state-resources');
+Route::get('/add-business', function () {
+    return Inertia::render('add-business', []);
+})->name('add-business');
+Route::get('/faq', function () {
+    return Inertia::render('faq', []);
+})->name('faq');
+Route::get('/contact', function () {
+    return Inertia::render('contact', []);
+})->name('contact');
+Route::get('/sitemap', function () {
+    return Inertia::render('sitemap', []);
+})->name('sitemap');
+Route::get('/accessibility', function () {
+    return Inertia::render('accessibility', []);
+})->name('accessibility');
 
 
 
