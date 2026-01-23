36edc36e3b2ef62ea1787e1d37999d2de2afcdab
Bob Ware <bware@sref.info>
2026-01-23 12:53:50 -0500

    update tsconfig and vite.config setttings to use tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     plugins: [
@@ -17,6 +18,7 @@ export default defineConfig({
             },
         }),
         tailwindcss(),
+        tsConfigPaths(),
         wayfinder({
             formVariants: true,
         }),
