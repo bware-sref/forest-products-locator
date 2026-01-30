e654304cb76f5455716de267957197a89916c7d9
Bob Ware <bware@sref.info>
2026-01-30 15:49:27 -0500

    refactor hero to make it more reusable

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -42,56 +42,40 @@ export default function Welcome() {
         <AppLayout>
             <Head title={pageTitle} />
             {/** 
-             * Hero must be in a full-width wrapper. 
+             * Hero must be in a full-width wrapper.
             */}
-            <div className="grid grid-cols-1 grid-rows-1 w-full">
-                {/* Hero is a wrapper for <picture> */}
-                <Hero
-                    src={heroFallback}
-                    alt="Lumber"
-                    pictureClassName={'col-start-1 row-start-1 h-full w-full object-cover'} />
-
-                {/** 
-                 * content of hero 
-                 * need full width wrapper to overlay the image
-                 * inside of full-width, define a content column that is only as wide as top-nav
-                 * this should probably be passed as contents to Hero component
-                 */}
-                <div className="hero-content w-full col-start-1 row-start-1 flex flex-col Xbg-sky-500/30">
-                    {/**
-                     * below is our content column matching the size of the top nav with mx-auto to center it
-                     * however, we may need yet another wrapper to constrain width of the children
-                     */}
-                    <div className="hero-content__inner mx-auto flex flex-col gap-8 h-full w-full lg:w-7xl max-w-[335px] lg:max-w-7xl items-start p-5 Xbg-amber-300/30">
-                        <div className="flex flex-col gap-8 max-w[335px] lg:max-w-3xl justify-self-center items-center-safe Xbg-red-500/30">
-                            <h1 className="text-5xl font-bold my-6">
-                                Welcome to the Primary Forest Products Locator
-                            </h1>
-                            <p className="text-[22px] my-5">
-                                The Primary Forest Products Locator is a tool provided by the <em className="italic">Southern Group of State Foresters</em> to assist buyers in locating primary wood product manufacturing companies.
-                            </p>
-                            <Button
-                                asChild
-                                size="lg"
-                                
-                                className="grow-0 place-self-start bg-coupe border-white border-2 text-2xl py-10"
-                            >
-                                <Link
-                                    href={millMap()}
-                                    className="grow-0"
-                                >
-                                    View Mill Map
-                                    <ArrowRightIcon 
-                                        data-icon="inline-end"
-                                        size={40}
-                                        className="w-8 h-8 size-8 ml-2"
-                                    />
-                                </Link>
-                            </Button>                                
-                        </div>
-                    </div>
+            <Hero
+                src={heroFallback}
+                alt="Lumber"
+                pictureClassName={'col-start-1 row-start-1 h-full w-full object-cover'}
+            >
+                <div className="flex flex-col gap-8 max-w[335px] lg:max-w-3xl justify-self-center items-center-safe Xbg-red-500/30">
+                    <h1 className="text-5xl font-bold my-6">
+                        Welcome to the Primary Forest Products Locator
+                    </h1>
+                    <p className="text-[22px] my-5">
+                        The Primary Forest Products Locator is a tool provided by the <em className="italic">Southern Group of State Foresters</em> to assist buyers in locating primary wood product manufacturing companies.
+                    </p>
+                    <Button
+                        asChild
+                        size="lg"                        
+                        className="grow-0 place-self-start bg-coupe border-white border-2 text-2xl py-10"
+                    >
+                        <Link
+                            href={millMap()}
+                            className="grow-0"
+                        >
+                            View Mill Map
+                            <ArrowRightIcon 
+                                data-icon="inline-end"
+                                size={40}
+                                className="w-8 h-8 size-8 ml-2"
+                            />
+                        </Link>
+                    </Button>                                
                 </div>
-            </div>
+            </Hero>
+
             {/**
              * Cards!
              */}
