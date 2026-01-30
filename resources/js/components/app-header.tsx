f0a6b1b752c0a6363b7b602f57d46adcfd0ea039
Bob Ware <bware@sref.info>
2026-01-30 15:50:49 -0500

    hide search in top nav

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -90,6 +90,8 @@ interface AppHeaderProps {
     breadcrumbs?: BreadcrumbItem[];
 }
 
+const displaySearch = false;
+
 export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
     const page = usePage<SharedData>();
     const { auth } = page.props;
@@ -211,47 +213,49 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
                     </div>
                     {/* new menu who dis?*/}
                     <div className="ml-auto flex items-center space-x-2">
-                        <div className="relative flex items-center space-x-1">
-                            <Button
-                                variant="ghost"
-                                size="icon"
-                                className="group h-9 w-9 cursor-pointer"
-                            >
-                                <Search className="!size-5 opacity-80 group-hover:opacity-100" />
-                            </Button>
-                            <div className="hidden lg:flex">
-                                {rightNavItems.map((item) => (
-                                    <TooltipProvider
-                                        key={item.title}
-                                        delayDuration={0}
-                                    >
-                                        <Tooltip>
-                                            <TooltipTrigger>
-                                                <a
-                                                    href={resolveUrl(item.href)}
-                                                    target="_blank"
-                                                    rel="noopener noreferrer"
-                                                    className="group ml-1 inline-flex h-9 w-9 items-center justify-center rounded-md bg-transparent p-0 text-sm font-medium text-accent-foreground ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50"
-                                                >
-                                                    <span className="sr-only">
-                                                        {item.title}
-                                                    </span>
-                                                    {item.icon && (
-                                                        <Icon
-                                                            iconNode={item.icon}
-                                                            className="size-5 opacity-80 group-hover:opacity-100"
-                                                        />
-                                                    )}
-                                                </a>
-                                            </TooltipTrigger>
-                                            <TooltipContent>
-                                                <p>{item.title}</p>
-                                            </TooltipContent>
-                                        </Tooltip>
-                                    </TooltipProvider>
-                                ))}
+                        { displaySearch ? (
+                            <div className="relative flex items-center space-x-1">
+                                <Button
+                                    variant="ghost"
+                                    size="icon"
+                                    className="group h-9 w-9 cursor-pointer"
+                                >
+                                    <Search className="!size-5 opacity-80 group-hover:opacity-100" />
+                                </Button>
+                                <div className="hidden lg:flex">
+                                    {rightNavItems.map((item) => (
+                                        <TooltipProvider
+                                            key={item.title}
+                                            delayDuration={0}
+                                        >
+                                            <Tooltip>
+                                                <TooltipTrigger>
+                                                    <a
+                                                        href={resolveUrl(item.href)}
+                                                        target="_blank"
+                                                        rel="noopener noreferrer"
+                                                        className="group ml-1 inline-flex h-9 w-9 items-center justify-center rounded-md bg-transparent p-0 text-sm font-medium text-accent-foreground ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50"
+                                                    >
+                                                        <span className="sr-only">
+                                                            {item.title}
+                                                        </span>
+                                                        {item.icon && (
+                                                            <Icon
+                                                                iconNode={item.icon}
+                                                                className="size-5 opacity-80 group-hover:opacity-100"
+                                                            />
+                                                        )}
+                                                    </a>
+                                                </TooltipTrigger>
+                                                <TooltipContent>
+                                                    <p>{item.title}</p>
+                                                </TooltipContent>
+                                            </Tooltip>
+                                        </TooltipProvider>
+                                    ))}
+                                </div>
                             </div>
-                        </div>
+                        ) : ('')}
                         {/**
                          * Okay this whole logged-in user crap is probably not necessary.
                          * But at minimum, we need to check if the user is logged in before blindly attempting to display it.
