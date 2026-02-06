38c74e43f4d0633d6590ed7884a9b91e65c65095
Bob Ware <bware@sref.info>
2026-02-06 10:39:03 -0500

    fix typo in MillTypePolicy class name

--- app/Policies/MillTypePolicy.php
+++ app/Policies/MillTypePolicy.php
@@ -6,7 +6,7 @@ use App\Models\MillType;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class MillTypesPolicy
+class MillTypePolicy
 {
     /**
      * Determine whether the user can view any models.
