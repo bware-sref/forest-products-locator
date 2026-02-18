30042d390d83e0d237bc1b4408cd4fe2a3e11078
Bob Ware <bware@sref.info>
2026-02-18 16:36:00 -0500

    fix lint issues

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -34,7 +34,7 @@ import { type BreadcrumbItem, type NavItem, type SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
 import { Menu, Search } from 'lucide-react';
 import AppLogo from './app-logo';
-import AppLogoIcon from './app-logo-icon';
+// import AppLogoIcon from './app-logo-icon';
 
 const mainNavItems: NavItem[] = [
     // {
