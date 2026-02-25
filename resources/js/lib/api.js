4240e8828518d92a77ef106c600823a7e4d35253
Bob Ware <bware@sref.info>
2026-02-25 16:57:50 -0500

    clean up

--- resources/js/lib/api.js
+++ resources/js/lib/api.js
@@ -1,19 +1,12 @@
 
 export async function fetchMills(url, params) {
-    // const text = params.text || null;
-    // const state = params.state || null;
-    // const county = params.county || null;
-    // const millTypes = params.millTypes || null;
-    // const woodSpecies = params.woodSpecies || null;
     const urlParams = new URLSearchParams(params);
-    console.log(urlParams.toString());   
     try {
         const response = await fetch(url + '?' + urlParams.toString());
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
         const result = await response.json();
-        // console.log(result);
         return result;
     } catch (error) {
         console.log(`Error with fetch: ${error.message}`);
