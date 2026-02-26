1e65bdda8f1982d719ce9538552e00b2287a00a0
Bob Ware <bware@sref.info>
2026-02-26 15:21:22 -0500

    Break Mill Filters into a separate component

---
+++ resources/js/components/mill-filters.tsx
@@ -0,0 +1,140 @@
+import { cn } from "@/lib/utils";
+import {
+    type County,
+    type MillType,
+    type State,
+    type WoodSpecies,
+    // type MillFiltersProps,
+} from '@/types';
+// import { 
+//     ChangeEventHandler,
+//     useEffect,
+//     useState,    
+// } from 'react';
+import {
+    Field,
+    FieldDescription,
+    FieldGroup,
+    FieldLabel,
+} from "@/components/ui/field";
+import {
+    InputGroup,
+    InputGroupAddon,
+    InputGroupButton,
+    InputGroupInput,
+} from "@/components/ui/input-group";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    // SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+// import { Button } from '@/components/ui/button';
+import {
+    SearchIcon
+} from 'lucide-react';
+
+/**
+ * MillFilters needs:
+ * - states, which we might want to just pluck with usePage
+ * - counties, which come attached to states
+ * - millTypes
+ * - woodSpecies
+ * - callbacks for
+ *  - onStateChange
+ *  - onCountyChange
+ *  - onMillType
+ *  - onWoodSpecies
+ */
+export interface MillFiltersProps {
+    headline?: string;
+    states: State[];
+    counties?: County[];
+    millTypes?: MillType[];
+    woodSpecies?: WoodSpecies[];
+    /**
+     * Probably need props for selected values of each select box.
+     */
+    // onTextSearchChange: (event: Event) => void;
+    onStateSelectChange: (event: Event|string) => void;
+    // onCountySelectChange: (event: Event) => void;
+    // onMillTypesSelectChange: (event: Event) => void;
+    // onWoodSpeciesSelectChange: (event: Event) => void;
+}
+
+export default function MillFilters({...props}: MillFiltersProps) {
+
+    const headline = props.headline || 'Mill List';
+    const states = props.states;
+    const counties = props.counties;
+    const millTypes = props.millTypes;
+    const woodSpecies = props.woodSpecies;
+
+    return (
+        <div className="flex w-full flex-row items-stretch max-w-83.75">
+            <div className="grid gap-1 bg-nature p-8 w-full">
+                <h2 className="text-xl font-bold text-beluga pb-2">{headline}</h2>
+                {/**
+                 * mess
+                 */}
+                <FieldGroup>
+                    {/* text search */}
+                    <Field>
+                        <FieldLabel className="sr-only" htmlFor="s">Text Search</FieldLabel>
+                        <InputGroup className="rounded-2xl bg-beluga dark:bg-beluga">
+                            <InputGroupInput 
+                                id="s"
+                                className="text-velvet dark:text-velvet"
+                                placeholder="Search mills..."
+                            />
+                            <InputGroupAddon align="inline-end">
+                                <InputGroupButton                            
+                                    aria-label="Search"
+                                    title="Search"
+                                    size="icon-sm"
+                                >
+                                    <SearchIcon />
+                                </InputGroupButton>
+                            </InputGroupAddon>
+                        </InputGroup>
+                    </Field>
+                    <Field>
+                        <FieldLabel 
+                            htmlFor="state"
+                            className="text-white"
+                            >State:</FieldLabel>
+                        <Select 
+                            defaultValue=""
+                            onValueChange={props.onStateSelectChange}                            
+                        >
+                            <SelectTrigger id="state"
+                                className="bg-beluga text-velvet"
+                            >
+                                <SelectValue 
+                                    placeholder="Select a state"
+                                    className="bg-beluga text-velvet"
+                                />
+                            </SelectTrigger>
+                            <SelectContent className="bg-beluga text-velvet">
+                                <SelectGroup className="bg-beluga text-velvet">
+                                    {states.length > 0 && states.map(state => 
+                                        <SelectItem 
+                                            key={state.abbreviation}
+                                            value={state.abbreviation}
+                                        >{state.name}</SelectItem>
+                                    )}
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </Field>
+                </FieldGroup>
+                <div>
+                </div>
+            </div>                            
+        </div>
+
+    );
+}
