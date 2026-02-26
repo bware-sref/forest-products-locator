e92538c0d8abef6d8bb9d4c6d46eaed7c6b06b04
Bob Ware <bware@sref.info>
2026-02-26 17:30:37 -0500

    update references

--- app/Http/Controllers/MillController.php
+++ app/Http/Controllers/MillController.php
@@ -35,7 +35,7 @@ class MillController extends Controller
         // also need to figure out distance filtering
         // probably something we can crib from the old version.
 
-        return Inertia::render('mill-list', [
+        return Inertia::render('mill-list-page', [
             // 'mills' => $mills->toArray(),
             /**
              * we can forego the counties by just loading them onto the states
