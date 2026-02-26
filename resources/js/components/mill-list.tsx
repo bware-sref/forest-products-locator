0650e774bbe3bdcda3638cfc0bcdcb8bd43c1b2a
Bob Ware <bware@sref.info>
2026-02-26 17:59:32 -0500

    Break mill list insto separate component

---
+++ resources/js/components/mill-list.tsx
@@ -0,0 +1,85 @@
+import { 
+    type County,
+    type Mill,
+    type MillType,
+    type State,
+    type WoodSpecies,
+} from '@/types';
+import { 
+    Link,
+} from '@inertiajs/react';
+import { 
+    ReactNode,
+    useEffect,
+    useState,    
+} from 'react';
+
+// import { Button } from '@/components/ui/button';
+// import {
+//     Card,
+//     CardContent,
+//     CardHeader,
+//     CardTitle,
+// } from "@/components/ui/card";
+import {
+    ArrowRight,
+    // SearchIcon
+} from 'lucide-react';
+import { show } from '@/actions/App/Http/Controllers/MillController';
+
+export interface MillListProps {
+    mills: Mill[];
+    children?: ReactNode;
+    [key: string]: unknown;
+}
+
+/**
+ * Maybe we want to allow children to inform the no mills state?
+ */
+export default function MillList({mills, children, ...props}: MillListProps) {
+
+    return (
+        <div className="flex flex-row w-83.75 max-w-83.75">
+            <ul className="flex flex-col justify-evenly items-stretch gap-1">                            
+                {(mills.length > 0) ? mills.map(mill => 
+                    <li className="bg-beluga text-black p-8 " key={mill.match_id}>
+                        <h2 className="font-extrabold text-velvet text-lg">
+                            <Link 
+                                href={show(mill.match_id)}
+                                className="hover:underline"
+                            >
+                                {mill.mill_name}
+                            </Link>
+                        </h2>
+                        <address>
+                            {mill.physical_address}
+                            <br />
+                            {mill.physical_address_two}
+                        </address>
+                        <p>
+                            <Link 
+                                href={`https://maps.google.com/?q=${mill.latitude},${mill.longitude}`}
+                                target="_blank"
+                            >
+                                Map This Location
+                            </Link>
+                        </p>
+                        <p><strong>Species: </strong> 
+                            {mill.wood_species ? mill.wood_species.map((wood, index) => {
+                                const prefix = (0 < index) ? ', ' : '';
+                                return prefix + wood.name;
+                            }) : ''}
+                        </p>
+                        <p><strong>Mill Type: </strong> {
+                            mill.mill_types ? mill.mill_types.map((millType, index) => {
+                                const prefix = (0 < index) ? ', ' : '';                                            
+                                return prefix + millType.name;
+                            }) : ''
+                        }</p>
+                    </li>
+                ) : (<li className="bg-beluga text-black p-8 min-h-screen w-83.75">Loading...</li>)}
+            </ul>
+
+        </div>
+    );
+}
