5765e3381ae9b0f14111f596429b74ee1a1b6f77
Bob Ware <bware@sref.info>
2026-03-17 13:45:47 -0400

    update useEffect for searchParams to be dependent on the state variables for the form elements

--- resources/js/pages/mill-list-page.tsx
+++ resources/js/pages/mill-list-page.tsx
@@ -109,12 +109,21 @@ export default function MillListPage() {
 
     // update to use state.id instead of state.abbreviation
     // also need to update the API and Request to accept the state ID instead
-    const handleStateSelectChange = function (optionValue: Event|string) {
+    const handleStateSelectChange = function (optionValue: string) {
+        console.log('handlingStateSelectChange...', {selectedState, optionValue});
         // searchParams should only be set if we find the state, no?
-        setSearchParams({state: optionValue});
+        // Setting searchParams becomes the thing that triggers the API call.
+        // also, we should only setSearchParams if we find the state value and the state value is different.
+        if (selectedState && selectedState.value == optionValue) {
+            console.log('selectedState has not changed...', {selectedState, optionValue});
+            return;
+        }
+
+        // pretty sure that removing setSearchParams is going to cause the mill list to stop updating...
+        // yep, good guess.
+        // setSearchParams({state: optionValue});
+
         for (const state of states) {
-            // if (state.abbreviation === stateAbbreviation) {
-            // if (String(state.id) === stateAbbreviation) {
             if (state.value === optionValue) {
                 setSelectedState(state);
                 setCounties(countiesByState[optionValue]);
@@ -122,36 +131,51 @@ export default function MillListPage() {
                  * do we need to update the millTypes and WoodSpecies?
                  * Only if we have millTypes and woodSpecies available by state.
                  */
+                console.log('selectedState!', selectedState);
                 break;
             }
         };
     }
 
-    const handleCountySelectChange = function (countyId: string) {
-        
+    /**
+     * This could probably be further simplified if selectedCounty was not a County
+     * @param countyId 
+     */
+    const handleCountySelectChange = function (countyId: string) {        
         console.log('in handleCountySelectChange...', countyId);
-        console.log('need to look up the County in the list of counties by state...or just in the list of counties...');
+        // console.log('need to look up the County in the list of counties by state...or just in the list of counties...');
         // or just in the list of counties...
         const county = counties.find((c) => c.id == parseInt(countyId)) || null;
         if (county !== selectedCounty) {
+            console.log('county !== selectedCounty, updating: ', {county, selectedCounty});
             setSelectedCounty(county);
         }
     }
 
+    /**
+     * Same old, probably simpler to make selectedMillType an int or string instead of a MillType
+     * @param millTypeId 
+     */
     const handleMillTypeSelectChange = function (millTypeId: string) {
         console.log('in handleMillTypeSelectChange...', millTypeId);
         const millType = page.props.millTypes ? 
             (page.props.millTypes.find((mt) => mt.id == parseInt(millTypeId)) || null) : null;
         if (millType !== selectedMillType) {
+            console.log('millType changed!', {millType, selectedMillType});
             setSelectedMillType(millType);
         }
     }
 
+    /**
+     * Same old, probably simpler to make selectedWoodSpecies an int or string instead of a WoodSpecies
+     * @param woodSpeciesId 
+     */
     const handleWoodSpeciesSelectChange = function (woodSpeciesId: string) {
         console.log('in handleWoodSpeciesSelectChange: ', woodSpeciesId);
         const woodSpecies = page.props.woodSpecies ? 
             (page.props.woodSpecies.find((w) => w.id == parseInt(woodSpeciesId)) || null) : null;
         if (woodSpecies !== selectedWoodSpecies) {
+            console.log('woodSpecies changed!', {woodSpecies, selectedWoodSpecies});
             setSelectedWoodSpecies(woodSpecies);
         }
     }
@@ -178,10 +202,12 @@ export default function MillListPage() {
          * fun fact, when running in development mode (i.e., npm run dev), React does all fetch requests twice!
          */
         let ignore = false;
+        // setting mills as empty list would trigger the "Loading" state of the millList
         // setMills([]);
         fetchMills(page.props.millsApiUrl, searchParams).then(result => {
             if (!ignore) {
                 // console.log('mills!', result);
+                // should we check for errors before setting the mill list?
                 setMills(result);
             }
         });
@@ -197,12 +223,62 @@ export default function MillListPage() {
     // es-lint says millsApiUrl and searchParams are dependencies of useEffect, so add them or remove the array.
     // This method does seem like it should watch searchParams rather than just selectedState, but maybe not?
     // if we use a submit button instead of immediately refreshing the mill list, this is not the same issue.
-    }, [selectedState]);
+    // }, [selectedState]);
+    }, [page.props.millsApiUrl, searchParams]);
 
+    // make this effect depend on all the selected input values
+    // q -> searchText
     useEffect(() => {
-        console.log('state changed!');
-        console.log('this means we need to update the counties, mill types, and wood species');
-    }, [selectedState]);
+        // just add all the values to searchParams and see what happens
+        // empty result happens
+        // we may need to check differences between current searchParams and the dependent values
+        console.log('when usingEffect to try to fetch mills...', {
+            searchParams,
+            searchText,
+            selectedState,
+            selectedCounty,
+            selectedMillType,
+            selectedWoodSpecies
+        });
+        const params: SearchParams = {};
+        // selectedValue, searchParamKey
+        // need to check existence of state variable, state variable value and searchParams.key
+        // before comparing to searchParams.
+        // do we even care if they're different from the searchParams values (if any)?
+        // probably not, but let's do overkill first
+        if (selectedState && selectedState.value) {// && searchParams.state && selectedState.value !== searchParams.state) {
+            params.state = selectedState.value;
+        }
+        if (selectedCounty && selectedCounty.value) { // && selectedCounty.value !== searchParams.county) {
+            params.county = selectedCounty.value;
+        }
+        if (selectedMillType && selectedMillType.value) {
+            params.millType = selectedMillType.value;
+        }
+        if (selectedWoodSpecies && selectedWoodSpecies.value) {
+            params.woodSpecies = selectedWoodSpecies.value;
+        }
+        // oh right, searchText is the odd ball WRT having a value member
+        if (searchText) {
+            params.q = searchText;
+        }
+        console.log('setting search params: ', params);
+        setSearchParams(params);
+        // setSearchParams({
+        //     q: searchText,
+        //     state: selectedState,
+        //     county: selectedCounty,
+        //     millType: selectedMillType,
+        //     woodSpecies: selectedWoodSpecies,
+        // });
+
+    }, [searchText, selectedState, selectedCounty, selectedMillType, selectedWoodSpecies]);
+
+
+    // useEffect(() => {
+    //     console.log('state changed!');
+    //     console.log('this means we need to update the counties, mill types, and wood species');
+    // }, [selectedState]);
 
     /**
      * Render!
@@ -220,9 +296,10 @@ export default function MillListPage() {
                 <div className="flex flex-col w-full max-w-7xl items-start justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0 px-5">
                     {/**
                      * Mill Filters
+                     * may want to rename MillFilters component textSearch parameter to match
                      */}
                     <MillFilters
-                        textSearch={textSearch}
+                        textSearch={searchText}
                         states={states}
                         counties={counties}
                         millTypes={page.props.millTypes}
