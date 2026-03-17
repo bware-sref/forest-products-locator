8a451a2a68956ada76b6ff161a2a394cf01988a4
Bob Ware <bware@sref.info>
2026-03-17 11:13:36 -0400

    es-lint

--- resources/js/lib/useDebounce.ts
+++ resources/js/lib/useDebounce.ts
@@ -12,17 +12,26 @@ import { debounce } from 'lodash-es';
  * @returns 
  */
 export default function useDebounce(callback: () => void, delay: number = 500) {
-    const ref = useRef<any>(null);
+    // es-lint change 'any' to 'unknown'
+    const ref = useRef<unknown>(null);
 
     useEffect(() => {
+        // es-lint hates this because not spozta access refs during render
+        // doing so may cause strange render behaviors
+        // the docs actually say, "can cause your component not to update as expected."
+        // But if we expect the issue might manifest, does that make the docs a liar?
         ref.current = callback;
     }, [callback]);
 
     const debouncedCallback = useMemo(() => {
         const func = () => {
-            ref.current?.();
+            if (ref.current && typeof ref.current == 'function') {
+                ref.current?.();
+            }
         };
         return debounce(func, delay);
+        // es-lint says 'delay' is a dependency and either add the dependency or remove the dependency array.
+        // I wonder how it will react to the latter...
     }, []);
 
     return debouncedCallback;
