edf082ce2095b6de06fbb0d3c1cdcf7b6e393ce3
Bob Ware <bware@sref.info>
2026-04-01 13:50:12 -0400

    add Toaster to app-layout to allow showing toasts

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -2,6 +2,7 @@ import AppLayoutTemplate from '@/layouts/app/app-header-layout';
 // import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
 import { type BreadcrumbItem } from '@/types';
 import { type ReactNode } from 'react';
+import { Toaster } from 'sonner';
 
 interface AppLayoutProps {
     children: ReactNode;
@@ -11,5 +12,6 @@ interface AppLayoutProps {
 export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => (
     <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
         {children}
+        <Toaster />
     </AppLayoutTemplate>
 );
