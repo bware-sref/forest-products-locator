018faf69163664c347539438ed5e7166edb05060
Bob Ware <bware@sref.info>
2026-04-03 09:38:04 -0400

    add centralized flash handler stub to app-layout

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -1,17 +1,27 @@
 import AppLayoutTemplate from '@/layouts/app/app-header-layout';
 // import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
 import { type BreadcrumbItem } from '@/types';
+import { type PageFlashData } from '@inertiajs/core';
 import { type ReactNode } from 'react';
-import { Toaster } from 'sonner';
+import { toast, Toaster } from 'sonner';
 
 interface AppLayoutProps {
     children: ReactNode;
     breadcrumbs?: BreadcrumbItem[];
+    flash?: PageFlashData;
 }
 
-export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => (
+/**
+ * Per InertiaJS docs, it seems like it would be good centralize the toasting flash data
+ */
+
+export default ({ children, breadcrumbs, flash, ...props }: AppLayoutProps) => (
     <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
         {children}
         <Toaster />
+        
+        <>
+        {flash && console.log('flash!', flash)}
+        </>
     </AppLayoutTemplate>
 );
