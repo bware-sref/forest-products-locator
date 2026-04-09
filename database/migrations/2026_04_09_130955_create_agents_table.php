5f8fb7d03c8d6b86049104d0eb10cf3d32a48448
Bob Ware <bware@sref.info>
2026-04-09 09:19:11 -0400

    add basic fields to Agent migration

--- database/migrations/2026_04_09_130955_create_agents_table.php
+++ database/migrations/2026_04_09_130955_create_agents_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('agents', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('title')->nullable();
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            /**
+             * I'm not sure if state_id should be nullable or not.
+             * Also, neither cascading on delete nor nulling on delete seems necessary because states are unlikely to be deleted, but nulling on delete is probably safer just in case.
+             */
+            $table->foreignId('state_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('street_address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('zip_code')->nullable();
             $table->timestamps();
         });
     }
