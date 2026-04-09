05b28e5228e98f7915edc094dd979a29f9126bb5
Bob Ware <bware@sref.info>
2026-04-09 09:25:45 -0400

    fluff Agent model

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -4,9 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Agent extends Model
 {
     /** @use HasFactory<\Database\Factories\AgentFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'title',
+        'email',
+        'phone',
+        'state_id',
+        'street_address',
+        'city',
+        'zip_code',
+    ];
+
+    public function state(): BelongsTo
+    {
+        return $this->belongsTo(State::class);
+    }
 }
