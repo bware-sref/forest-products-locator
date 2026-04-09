af28908cf0e7d770b7fb10420dc30dbdc4de4965
Bob Ware <bware@sref.info>
2026-04-09 09:58:51 -0400

    fluff AgentSeeder to create an Agent for each southern state

--- database/seeders/AgentSeeder.php
+++ database/seeders/AgentSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Agent;
+use App\Models\State;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,19 @@ class AgentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        /**
+         * Ideally we'll end up with at least one agent for each southern state.
+         * And realisitcally, each state will have multiple agents.
+         * But that sort of begs the question of MillEdits and new Mill submissions getting assigned to the correct agent. So for now, we'll just create one agent for each state.
+         */
+        $stateIds = State::whereIn('abbreviation', State::$southernStates)->pluck('id')->toArray();
+        foreach ($stateIds as $stateId) {
+            /**
+             * Should we create a random number of agents for each state? Or just one? For now, just one, but we can easily change this in the future if we want to test out the functionality with multiple agents per state.
+             */
+            Agent::factory()->create([
+                'state_id' => $stateId,
+            ]);
+        }
     }
 }
