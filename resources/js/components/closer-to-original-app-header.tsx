f7b274837a62e68d95246b09212079728f02ede5
Bob Ware <bware@sref.info>
2026-04-13 14:26:13 -0400

    make a copy of the app header with all the extra stuff we don't use in it

---
+++ resources/js/components/closer-to-original-app-header.tsx
@@ -0,0 +1,334 @@
+import { useState } from 'react';
+import { Breadcrumbs } from '@/components/breadcrumbs';
+import { Icon } from '@/components/icon';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuList,
+    navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from '@/components/ui/sheet';
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { UserMenuContent } from '@/components/user-menu-content';
+// import { useInitials } from '@/hooks/use-initials';
+import { cn, isSameUrl, resolveUrl } from '@/lib/utils';
+import { home, millMap, millList, stateResources, addBusiness, faq, contact } from '@/routes';
+import { type BreadcrumbItem, type NavItem, type SharedData } from '@/types';
+import { Link, usePage } from '@inertiajs/react';
+import { Menu, Search, XIcon } from 'lucide-react';
+import AppLogo from './app-logo';
+// import AppLogoIcon from './app-logo-icon';
+
+const mainNavItems: NavItem[] = [
+    // {
+    //     title: 'Dashboard',
+    //     href: dashboard(),
+    //     icon: LayoutGrid,
+    // },
+    {
+        title: 'Mill Map',
+        href: millMap(),
+        // icon: Map,
+    },
+    {
+        title: 'Mill List',
+        href: millList(),
+        // icon: List,
+    },
+    {
+        title: 'State Resources',
+        href: stateResources(),
+    },
+    {
+        title: 'Add Your Business',
+        href: addBusiness(),
+    },
+    {
+        title: 'FAQ',
+        href: faq(),
+    },
+    {
+        title: 'Contact',
+        href: contact(),
+    },
+];
+
+const rightNavItems: NavItem[] = [
+    // {
+    //     title: 'Repository',
+    //     href: 'https://github.com/laravel/react-starter-kit',
+    //     icon: Folder,
+    // },
+    // {
+    //     title: 'Documentation',
+    //     href: 'https://laravel.com/docs/starter-kits#react',
+    //     icon: BookOpen,
+    // },
+];
+
+const activeItemStyles =
+    'text-velvet-900 dark:bg-velvet-800 dark:text-velvet-100';
+
+interface AppHeaderProps {
+    breadcrumbs?: BreadcrumbItem[];
+}
+
+const displaySearch = false;
+
+export function CloserToOriginalAppHeader({ breadcrumbs = [] }: AppHeaderProps) {
+    const page = usePage<SharedData>();
+    const { auth } = page.props;
+    
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <>
+            <div className="border-b-6 border-sidebar-border bg-zucchini text-velvet">
+                <div className="mx-auto flex h-20 lg:h-20 items-center px-4 md:max-w-7xl">
+
+                    {/* Logo + home link */}
+                    <Link
+                        href={home()}
+                        prefetch
+                        className="flex items-center space-x-2"
+                    >
+                        <AppLogo />
+                    </Link>
+
+                    {/* Mobile Menu */}
+                    <div className="lg:hidden">
+                        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+                            <SheetTrigger asChild>
+                                <Button
+                                    variant="ghost"
+                                    size="lg"
+                                    className="mr-2 h-8.5 w-8.5 [&_svg:not([class*='size-'])]:size-8"
+                                >
+                                    {isOpen ? (
+                                        <XIcon className="h-5 w-5" />
+                                    ) : (
+                                        <Menu className="h-5 w-5" />
+                                    )}
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent
+                                side="left"
+                                className="flex h-full w-full flex-col items-stretch justify-between bg-sidebar mt-21"
+                                overlayClassName="bg-transparent"
+                                showCloseButton={false}
+                            >
+                                {/** Laravel includes the SheetTitle */}
+                                <SheetTitle className="sr-only">
+                                    Navigation Menu
+                                </SheetTitle>
+                                {/** However, browsers complain about a missing description, let's go redundant! */}
+                                <SheetDescription className="sr-only">
+                                    Main navigation links.
+                                </SheetDescription>
+                                {/**
+                                 * This header just repeats the logo.
+                                <SheetHeader className="flex justify-start text-left">
+                                    <Link
+                                        href={home()}
+                                        prefetch
+                                        className="flex items-center space-x-2"
+                                    >
+                                        <AppLogo />
+                                    </Link>
+                                </SheetHeader>
+                                */}
+
+                                <div className="flex h-full flex-1 flex-col space-y-4 p-4">
+                                    <div className="flex h-full flex-col justify-between text-sm">
+                                        <div className="flex flex-col space-y-4">
+                                            {mainNavItems.map((item) => (
+                                                <Link
+                                                    key={item.title}
+                                                    href={item.href}
+                                                    className="flex items-center space-x-2 font-bold text-3xl"
+                                                >
+                                                    {item.icon && (
+                                                        <Icon
+                                                            iconNode={item.icon}
+                                                            className="h-5 w-5"
+                                                        />
+                                                    )}
+                                                    <span>{item.title}</span>
+                                                </Link>
+                                            ))}
+                                        </div>
+
+                                        <div className="flex flex-col space-y-4">
+                                            {rightNavItems.map((item) => (
+                                                <a
+                                                    key={item.title}
+                                                    href={resolveUrl(item.href)}
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                    className="flex items-center space-x-2 font-medium"
+                                                >
+                                                    {item.icon && (
+                                                        <Icon
+                                                            iconNode={item.icon}
+                                                            className="h-5 w-5"
+                                                        />
+                                                    )}
+                                                    <span>{item.title}</span>
+                                                </a>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+                            </SheetContent>
+                        </Sheet>
+                    </div>
+
+                    {/** what happens if we move the logo before the mobile nav? */}
+                    {/* <Link
+                        href={home()}
+                        prefetch
+                        className="flex items-center space-x-2"
+                    >
+                        <AppLogo />
+                    </Link> */}
+
+                    {/* Desktop Navigation */}
+                    <div className="ml-auto hidden h-full w-auto items-stretch justify-stretch space-x-6 justify-items-end justify-self-end lg:flex">
+                        <NavigationMenu className="flex h-full items-stretch justify-stretch justify-self-end">
+                            <NavigationMenuList className="flex h-full items-stretch space-x-2 justify-items-end">
+                                {mainNavItems.map((item, index) => (
+                                    <NavigationMenuItem
+                                        key={index}
+                                        className="relative flex h-full items-center"
+                                    >
+                                        <Link
+                                            href={item.href}
+                                            className={cn(
+                                                navigationMenuTriggerStyle(),
+                                                isSameUrl(
+                                                    page.url,
+                                                    item.href,
+                                                ) && activeItemStyles,
+                                                'h-9 cursor-pointer px-3 bg-none font-bold text-xl',
+                                            )}
+                                        >
+                                            {item.icon && (
+                                                <Icon
+                                                    iconNode={item.icon}
+                                                    className="mr-2 h-4 w-4"
+                                                />
+                                            )}
+                                            {item.title}
+                                        </Link>
+                                        {isSameUrl(page.url, item.href) && (
+                                            <div className="absolute bottom-0.5 left-0 h-0.5 w-full translate-y-px bg-velvet dark:bg-velvet"></div>
+                                        )}
+                                    </NavigationMenuItem>
+                                ))}
+                            </NavigationMenuList>
+                        </NavigationMenu>
+                    </div>
+
+                    {/* new menu who dis?*/}
+                    <div className="ml-auto flex items-center space-x-2">
+                        { displaySearch ? (
+                            <div className="relative flex items-center space-x-1">
+                                <Button
+                                    variant="ghost"
+                                    size="icon"
+                                    className="group h-9 w-9 cursor-pointer"
+                                >
+                                    <Search className="size-5! opacity-80 group-hover:opacity-100" />
+                                </Button>
+                                <div className="hidden lg:flex">
+                                    {rightNavItems.map((item) => (
+                                        <TooltipProvider
+                                            key={item.title}
+                                            delayDuration={0}
+                                        >
+                                            <Tooltip>
+                                                <TooltipTrigger>
+                                                    <a
+                                                        href={resolveUrl(item.href)}
+                                                        target="_blank"
+                                                        rel="noopener noreferrer"
+                                                        className="group ml-1 inline-flex h-9 w-9 items-center justify-center rounded-md bg-transparent p-0 text-sm font-medium text-accent-foreground ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50"
+                                                    >
+                                                        <span className="sr-only">
+                                                            {item.title}
+                                                        </span>
+                                                        {item.icon && (
+                                                            <Icon
+                                                                iconNode={item.icon}
+                                                                className="size-5 opacity-80 group-hover:opacity-100"
+                                                            />
+                                                        )}
+                                                    </a>
+                                                </TooltipTrigger>
+                                                <TooltipContent>
+                                                    <p>{item.title}</p>
+                                                </TooltipContent>
+                                            </Tooltip>
+                                        </TooltipProvider>
+                                    ))}
+                                </div>
+                            </div>
+                        ) : ('')}
+                        {/**
+                         * Okay this whole logged-in user crap is probably not necessary.
+                         * But at minimum, we need to check if the user is logged in before blindly attempting to display it.
+                         */}
+                        {auth && auth.user ? (
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button
+                                    variant="ghost"
+                                    className="size-10 rounded-full p-1"
+                                >
+                                    <Avatar className="size-8 overflow-hidden rounded-full">
+                                        <AvatarImage
+                                            src={auth.user.avatar}
+                                            alt={auth.user.name}
+                                        />
+                                        <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                                            
+                                        </AvatarFallback>
+                                    </Avatar>
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-56" align="end">
+                                <UserMenuContent user={auth.user} />
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                        ) : ('')}
+                    </div>
+                </div>
+            </div>
+            {breadcrumbs.length > 1 && (
+                <div className="flex w-full border-b border-sidebar-border/70">
+                    <div className="mx-auto flex h-12 w-full items-center justify-start px-4 text-neutral-500 md:max-w-7xl">
+                        <Breadcrumbs breadcrumbs={breadcrumbs} />
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
