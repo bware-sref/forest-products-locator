39275d3e56c5ddb63a6cb09abd8c8a050a466abe
Bob Ware <bware@sref.info>
2026-04-14 12:55:05 -0400

    wrap mill list in a ScrollArea component

--- resources/js/components/mill-list.tsx
+++ resources/js/components/mill-list.tsx
@@ -1,6 +1,9 @@
 import { 
     type MillListProps,
 } from '@/types';
+import {
+    ScrollArea,
+} from '@/components/ui/scroll-area';
 import MillListItem from '@/components/mill-list-item';
 
 
@@ -11,16 +14,17 @@ import MillListItem from '@/components/mill-list-item';
 export default function MillList({mills, children, ...props}: MillListProps) {
 
     return (
-        <div className="flex flex-row w-85 max-w-89" {...props}>
-            <ul className="flex flex-col justify-evenly items-stretch gap-6">                            
-                {(mills && mills.length > 0) ? mills.map(mill => 
-                    <li className="p-0 w-full" key={mill.match_id}>
-                        <MillListItem mill={mill} />
-                    </li>
-                ) : (<li className="bg-beluga text-black p-8 min-h-screen w-83.75">Loading...</li>)}
-                {children}
-            </ul>
-
-        </div>
+        // <div className="flex flex-row w-85 max-w-89" {...props}>
+            <ScrollArea className="flex flex-row w-85 max-w-89 h-200">
+                <ul className="flex flex-col justify-evenly items-stretch gap-6">                            
+                    {(mills && mills.length > 0) ? mills.map(mill => 
+                        <li className="p-0 w-full" key={mill.match_id}>
+                            <MillListItem mill={mill} />
+                        </li>
+                    ) : (<li className="bg-beluga text-black p-8 min-h-screen w-83.75">Loading...</li>)}
+                    {children}
+                </ul>
+            </ScrollArea>
+        // </div>
     );
 }
