70ea6f698d649784f7647d66dbbe94322d8cb066
Bob Ware <bware@sref.info>
2026-04-15 15:52:14 -0400

    and mill list

--- resources/js/components/mill-list.tsx
+++ resources/js/components/mill-list.tsx
@@ -15,13 +15,13 @@ export default function MillList({mills, children, ...props}: MillListProps) {
 
     return (
         // <div className="flex flex-row w-85 max-w-89" {...props}>
-            <ScrollArea className="flex flex-row w-89 max-w-89 h-200 bg-lorne px-4">
+            <ScrollArea className="flex flex-row w-89 max-w-89 h-200 lg:bg-lorne lg:px-4">
                 <ul className="flex flex-col justify-evenly items-stretch gap-6">                            
                     {(mills && mills.length > 0) ? mills.map(mill => 
                         <li className="p-0 w-full" key={mill.match_id}>
                             <MillListItem mill={mill} />
                         </li>
-                    ) : (<li className="bg-beluga text-black p-8 min-h-screen w-83.75">Loading...</li>)}
+                    ) : (<li className="bg-beluga text-black lg:p-4 min-h-screen w-89">Loading...</li>)}
                     {children}
                 </ul>
             </ScrollArea>
