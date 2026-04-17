be84cb2062590bc7c615571b124984003da84aa1
Bob Ware <bware@sref.info>
2026-04-17 10:40:22 -0400

    add basic FAQ styles

--- resources/js/pages/faqs.tsx
+++ resources/js/pages/faqs.tsx
@@ -1,27 +1,39 @@
 import AppLayout from '@/layouts/app-layout';
 import { type SharedData } from '@/types';
 import { Head, usePage } from '@inertiajs/react';
-import { IFaq, IFaqCategory } from '@/types';
+import { IFaqCategory } from '@/types';
 
 export default function Faqs() {
     const page = usePage<SharedData>();
     const pageTitle = 'FAQs';
-    // temporary work-around for unused properties lint
-    // page.props.pageTitle = pageTitle;
     const faqsByCategory: IFaqCategory[] = page.props.faqsByCategory as IFaqCategory[];
 
-    console.log(faqsByCategory);
+    // console.log(faqsByCategory);
 
     return (
         <AppLayout>
             <Head title={pageTitle} />
             <div className="flex min-h-screen flex-col items-center bg-nature p-6 text-beluga lg:justify-center lg:p-8 dark:bg-nature">
-                <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
-                    <h1>{pageTitle}</h1>
-                    <ul>
+                <div className="flex flex-col w-full lg:max-w-7xl items-center lg:items-start justify-start opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0 px-6 gap-5">
+                    <h1 className="font-bold text-3xl">{pageTitle}</h1>
+                    <ul className="flex flex-col gap-4">
                         {faqsByCategory.map((category) => (
-                            <li key={category.slug} >
-                                {category.name}
+                            <li key={category.slug}
+                                className="border-b last:border-0"
+                            >
+                                <h2 className="font-bold text-xl mb-4">
+                                    {category.name}
+                                </h2>
+                                <ul className="flex flex-col gap-4">
+                                    {category.faqs.map((faq) => (
+                                        <li key={faq.id}
+                                            className="odd:bg-lorne even:bg-coupe p-4"
+                                        >
+                                            <div className="question mb-2">{faq.question}</div>
+                                            <div className="answer">{faq.answer}</div>
+                                        </li>
+                                    ))}
+                                </ul>
                             </li>
                         ))}
                     </ul>
