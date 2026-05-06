6f907d0898ed5154eeed6b0d65d5d5d2ec2c91f0
Bob Ware <bware@sref.info>
2026-05-06 14:49:08 -0400

    Adjust content column width to position h1 above forms

--- resources/js/pages/contact.tsx
+++ resources/js/pages/contact.tsx
@@ -15,7 +15,7 @@ export default function Contact() {
         <AppLayout>
             <Head title={pageTitle} />
             <div className="flex min-h-180 flex-col items-center bg-nature p-6 text-beluga lg:justify-center lg:p-8 dark:bg-nature">
-                <div className="flex flex-col w-full items-start justify-start opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0 lg:max-w-7xl py-8 px-8">                    
+                <div className="flex flex-col w-full items-start justify-start opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0 lg:max-w-lg lg:py-3 px-8">                    
                     <h1 className="text-3xl font-bold mb-3">{pageTitle}</h1>
                     <ContactForm />
                 </div>
