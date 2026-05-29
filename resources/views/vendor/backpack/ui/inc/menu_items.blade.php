9c07f564382aee982bf3287df5945a457251bce8
Bob Ware <bware@sref.info>
2026-05-29 15:16:06 -0400

    add basic permission checks to admin menu

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,5 +1,5 @@
 {{-- This file is used for menu items by any Backpack v7 theme --}}
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}-"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
 <x-backpack::menu-dropdown title="Mills" icon="la la-group">
     <x-backpack::menu-dropdown-item title="Manage Mills" icon="la la-question" :link="backpack_url('mill')" />
@@ -16,11 +16,19 @@
     <x-backpack::menu-dropdown-item title="FAQs" icon="la la-question" :link="backpack_url('faq')" />
     <x-backpack::menu-dropdown-item title="FAQ Categories" icon="la la-question" :link="backpack_url('faq-category')" />
 </x-backpack::menu-dropdown>
+@if(backpack_user()->can('view statistics'))
 <x-backpack::menu-dropdown title="Stats" icon="la la-group">
     <x-backpack::menu-dropdown-item title="Statistics" icon="la la-question" :link="backpack_url('statistics')" />
     <x-backpack::menu-dropdown-item title="Updated" icon="la la-question" :link="backpack_url('statistics/updated')" />
     <x-backpack::menu-dropdown-item title="Additions" icon="la la-question" :link="backpack_url('statistics/additions')" />
 </x-backpack::menu-dropdown>
+@endif
 <x-backpack::menu-dropdown title="Users" icon="la la-group">
     <x-backpack::menu-dropdown-item title="Users" icon="la la-question" :link="backpack_url('user')" />
+    @if(backpack_user()->can('edit roles'))
+    <x-backpack::menu-dropdown-item title="Roles" icon="la la-question" :link="backpack_url('role')" />
+    @endif
+    @if(backpack_user()->can('edit permissions'))
+    <x-backpack::menu-dropdown-item title="Permissions" icon="la la-question" :link="backpack_url('permission')" />
+    @endif
 </x-backpack::menu-dropdown>
