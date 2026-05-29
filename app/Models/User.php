71d294eecac34a0b51ee346c00fc0e2ae436340c
Bob Ware <bware@sref.info>
2026-05-29 15:15:52 -0400

    add permission trait to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 // use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 /**
  * @property int $id
@@ -49,6 +50,7 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     // use HasFactory, Notifiable, TwoFactorAuthenticatable, HasApiTokens;
     use HasFactory, Notifiable, HasApiTokens;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
