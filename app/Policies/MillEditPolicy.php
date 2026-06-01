2a4730805abc4ef528fb1bb18aa5cc6dd5cb97e0
Bob Ware <bware@sref.info>
2026-06-01 14:09:01 -0400

    rename MillEditsPolicy to MillEditPolicy

--- app/Policies/MillEditPolicy.php
+++ app/Policies/MillEditPolicy.php
@@ -2,11 +2,11 @@
 
 namespace App\Policies;
 
-use App\Models\MillEdits;
+use App\Models\MillEdit;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class MillEditsPolicy
+class MillEditPolicy
 {
     /**
      * Determine whether the user can view any models.
@@ -19,7 +19,7 @@ class MillEditsPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, MillEdits $millEdits): bool
+    public function view(User $user, MillEdit $millEdit): bool
     {
         return false;
     }
@@ -35,7 +35,7 @@ class MillEditsPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, MillEdits $millEdits): bool
+    public function update(User $user, MillEdit $millEdit): bool
     {
         return false;
     }
@@ -43,7 +43,7 @@ class MillEditsPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, MillEdits $millEdits): bool
+    public function delete(User $user, MillEdit $millEdit): bool
     {
         return false;
     }
@@ -51,7 +51,7 @@ class MillEditsPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, MillEdits $millEdits): bool
+    public function restore(User $user, MillEdit $millEdit): bool
     {
         return false;
     }
@@ -59,7 +59,7 @@ class MillEditsPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, MillEdits $millEdits): bool
+    public function forceDelete(User $user, MillEdit $millEdit): bool
     {
         return false;
     }
