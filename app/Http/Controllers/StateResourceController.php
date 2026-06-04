a6f998b4c8143cf2ed6a5ca1c7161f3137785d41
Bob Ware <bware@sref.info>
2026-06-04 10:35:48 -0400

    add logic for state resource index

--- app/Http/Controllers/StateResourceController.php
+++ app/Http/Controllers/StateResourceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\State;
 use App\Models\StateResource;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class StateResourceController extends Controller
 {
@@ -14,11 +15,31 @@ class StateResourceController extends Controller
      * byState() page.
      */
     public function index()
-    {}
+    {
+        /**
+         * Maybe need a scope for StateResources similar to Mills?
+         * Only pull published by default.
+         */
+        $states = State::select(['id', 'name', 'abbreviation', 'resource_summary'])
+            ->has('stateResources')
+            ->orderBy('name')
+            ->get();
+        return Inertia::render('state-resources', [
+            'pageTitle' => 'State Resources',
+            'states' => $states,
+        ]);
+    }
 
-    public function show(StateResource $stateResource)
-    {}
-
+    /**
+     * Fetch & display resources for the given state.
+     * On the front, the URLs include the state name (or abbreviation)
+     */
     public function byState(State $state)
+    {}
+
+    /**
+     * Fetch & display a given StateResource
+     */
+    public function show(State $state, StateResource $stateResource)
     {}
 }
