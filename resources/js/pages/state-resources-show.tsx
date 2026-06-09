75b3d21c9f4ce66dc717c976f31c892028169987
Bob Ware <bware@sref.info>
2026-06-09 10:45:39 -0400

    use DOMPurify to santize State Resource WYSIWYG content

--- resources/js/pages/state-resources-show.tsx
+++ resources/js/pages/state-resources-show.tsx
@@ -1,10 +1,12 @@
 import AppLayout from "@/layouts/app-layout";
+import DOMPurify from 'dompurify';
 import {
     State,
     StateResource,
 } from '@/types';
 import {
     Head,
+    Link,
     usePage,
 } from '@inertiajs/react';
 
@@ -15,6 +17,9 @@ export default function StateResourcesShow() {
         resource: StateResource;
     }>();
 
+    const content = DOMPurify.sanitize(page.props.resource.content);
+
+
     return (
         <AppLayout>
             <Head title={page.props.pageTitle} />
@@ -23,10 +28,28 @@ export default function StateResourcesShow() {
              * Probably should have made a content column component a while back...
              */}
             <div className="mx-auto flex flex-col max-w-full md:w-6xl lg:w-7xl xl:max-w-7xl">
-                <div className="flex flex-col max-w-83.75 lg:max-w-3xl px-6 text-beluga">
+                <div className="flex flex-col lg:flex-row max-w-83.75 lg:max-w-7xl px-6 text-beluga items-center">
                     <h1 className="text-3xl lg:text-5xl leading-10 font-bold mt-8 mb-6 w-full text-beluga">{page.props.resource.title}</h1>
-                    <div className="state-resource-content text-xl">
-                        {page.props.resource.content}
+                    {/**
+                     * @TODO: add link back to this state's resources
+                     */}
+                    <Link className="underline hover:no-underline" 
+                        href={'/state-resources/' + page.props.state.slug}>
+                        Back to {page.props.state.name} Resources
+                    </Link>
+                </div>
+                <div className="flex flex-col max-w-83.75 lg:max-w-3xl px-6 text-beluga">
+                    {/**
+                     * Outputting raw HTML is weird in React...
+                     */}
+                    <div 
+                        className="state-resource-content text-xl"
+                        dangerouslySetInnerHTML={{ __html: content }}
+                    >
+                        {/**
+                         * We may need to output raw content since CKEditor may include HTML...
+                         */}
+                        {/* {page.props.resource.content} */}
                     </div>
                 </div>
             </div>
