3f1213d1bcb2eb1fcc4977b47075e145bdded6f5
Bob Ware <bware@sref.info>
2026-06-17 14:40:50 -0400

    publish import translation files

---
+++ resources/lang/vendor/backpack/import-operation/en/import.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'import' => 'Import',
+    'confirm_selection' => 'Confirm Selection',
+    'into_field' => 'Into Field',
+    'import_data_from' => 'Import Data From',
+    'map_fields' => 'Map Fields',
+    'map_fields_for' => 'Map Fields For',
+    'confirm_mapping' => 'Confirm Mapping',
+    'select_a_file' => 'Select a file to import',
+    'accepted_types' => 'Upload a .csv, .xlsx, .xls file',
+    'select_a_column' => 'Select a file column',
+    'dont_import' => "Don't Import",
+    'primary_key' => 'Primary Key',
+    'primary_key_not_found' => "Unable to find a primary key for :model, please specify a column with 'primary_key' => true or add a column with your model's primary key.",
+    'please_map_at_least_one' => "Please map at least one file column to a field.",
+    'please_map_the_primary_key' => "Please map a file column to the primary key.",
+    'confirm_import' => "Confirm Import",
+    'confirm_your_import' => 'Confirm your Import Settings',
+    'click_here_to_download_file' => 'Click here to download your import file',
+    'remap_import' => 'Remap Import',
+    'your_import_has_been_queued' => 'Your import has been queued and will be processed in the background.',
+    'your_import_has_been_processed' => 'Your import has been processed successfully.',
+    'cant_find_log' => 'The specified import could not be found.',
+    'download_example' => 'Click here to download an example file.',
+    'text' => 'Text',
+    'number' => 'Number',
+    'date' => 'Date',
+    'boolean' => 'Boolean',
+    'array' => 'Array'
+];
