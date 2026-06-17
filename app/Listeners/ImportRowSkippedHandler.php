29576674e5e213694bd55e7dae40aa007962d6c7
Bob Ware <bware@sref.info>
2026-06-17 14:49:39 -0400

    attempt to increment skipped rows in the event

--- app/Listeners/ImportRowSkippedHandler.php
+++ app/Listeners/ImportRowSkippedHandler.php
@@ -22,9 +22,13 @@ class ImportRowSkippedHandler
      */
     public function handle(ImportRowSkipped $event): void
     {
+        $event->import_log->failed_rows += 1;
+        $event->import_log->save();
+
         Log::debug('ImportRowSkipped!', [
             'import_log.id' => $event->import_log->id,
             'row_data' => $event->row_data ?? 'No row data on skipped row!?!',
+            'failed_so_far' => $event->import_log->failed_rows,
         ]);
     }
 }
