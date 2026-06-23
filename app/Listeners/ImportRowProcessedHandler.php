dd3ec97b9cacd84d305a877cae41c181bfc73f69
Bob Ware <bware@sref.info>
2026-06-23 13:25:48 -0400

    suppress debug log messages

--- app/Listeners/ImportRowProcessedHandler.php
+++ app/Listeners/ImportRowProcessedHandler.php
@@ -28,7 +28,7 @@ class ImportRowProcessedHandler
         //     'importId' => $event->import_log->id,
         //     'updatedAt' => $event->import_log->updated_at->toDateTimeString(),
         // ]);
-        $staleValue = $event->import_log->processed_rows;
+        // $staleValue = $event->import_log->processed_rows;
 
         /**
          * Use refresh() to make sure the log is...fresh.
@@ -36,28 +36,26 @@ class ImportRowProcessedHandler
          */
         $event->import_log->refresh();
         
-        Log::debug('ImportRowProcessed after refreshing the log but before incrementing processed_rows: ', [
-            'importId' => $event->import_log->id,
-            'entryId' => $event->entry->id,
-            'staleProcessedRows' => $staleValue,
-            'processRows' => $event->import_log->processed_rows,
-            'updatedAt' => $event->import_log->updated_at->toDateTimeString(),
-        ]);
+        // Log::debug('ImportRowProcessed after refreshing the log but before incrementing processed_rows: ', [
+        //     'importId' => $event->import_log->id,
+        //     'entryId' => $event->entry->id,
+        //     'staleProcessedRows' => $staleValue,
+        //     'processRows' => $event->import_log->processed_rows,
+        //     'updatedAt' => $event->import_log->updated_at->toDateTimeString(),
+        // ]);
 
         /**
          * Phucking Intelephense phailing to discern default values...
          */
         $event->import_log->increment('processed_rows', amount: 1);
-        // $event->import_log->processed_rows += 1;
-        // $event->import_log->save();
 
-        Log::debug('ImportRowProcessed after incrementing processed_rows!', [
-            // 'entry' => $event->entry ?? 'no entry on event?!?',
-            'entryId' => $event->entry->id,
-            // 'row_data' => $event->row_data ?? 'no row_data on event?!?',
-            'processedRows' => $event->import_log->processed_rows,
-            'importId' => $event->import_log->id,
-            'updatedAt' => $event->import_log->updated_at->toDateTimeString(),
-        ]);
+        // Log::debug('ImportRowProcessed after incrementing processed_rows!', [
+        //     'importId' => $event->import_log->id,
+        //     // 'entry' => $event->entry ?? 'no entry on event?!?',
+        //     'entryId' => $event->entry->id,
+        //     // 'row_data' => $event->row_data ?? 'no row_data on event?!?',
+        //     'processedRows' => $event->import_log->processed_rows,
+        //     'updatedAt' => $event->import_log->updated_at->toDateTimeString(),
+        // ]);
     }
 }
