bb11f22cd8971f33555683d1daf49c06380bc90e
Bob Ware <bware@sref.info>
2026-06-29 16:00:09 -0400

    add new translation strings sort the damn keys to make everything easier to find

--- resources/lang/vendor/backpack/import-operation/en/import.php
+++ resources/lang/vendor/backpack/import-operation/en/import.php
@@ -1,33 +1,45 @@
 <?php
 
 return [
-    'import' => 'Import',
-    'confirm_selection' => 'Confirm Selection',
-    'into_field' => 'Into Database Field',
-    'import_data_from' => 'Import Data From File',
-    'map_fields' => 'Map Fields',
-    'map_fields_for' => 'Map Fields For',
-    'confirm_mapping' => 'Confirm Mapping',
-    'select_a_file' => 'Select a file to import',
     /**
      * .xls cannot be queued because of some issue with non-Unicode characters.
      * As such, we don't want to allow .xls files.
      */
     'accepted_types' => 'Upload a .csv or an .xlsx file',
-    'select_a_column' => 'Select a file column',
+    'cant_find_log' => 'The specified import could not be found.',
+    'click_here_to_download_file' => 'Click here to download your import file',
+    'confirm_import' => "Confirm Import",
+    'confirm_mapping' => 'Confirm Mapping',
+    'confirm_selection' => 'Confirm Selection',
+    'confirm_your_import' => 'Confirm your Import Settings',
     'dont_import' => "Don't Import",
-    'primary_key' => 'Primary Key',
-    'primary_key_not_found' => "Unable to find a primary key for :model, please specify a column with 'primary_key' => true or add a column with your model's primary key.",
+    'download_example' => 'Click here to download an example file.',
+    'import' => 'Import',
+    'import_data' => 'Import Data',
+    'import_data_from' => 'Import Data From File',
+    'into_field' => 'Into Database Field',
+    'map_fields' => 'Map Fields',
+    'map_fields_for' => 'Map Fields For',
     'please_map_at_least_one' => "Please map at least one file column to a field.",
     'please_map_the_primary_key' => "Please map a file column to the primary key.",
-    'confirm_import' => "Confirm Import",
-    'confirm_your_import' => 'Confirm your Import Settings',
-    'click_here_to_download_file' => 'Click here to download your import file',
+    'primary_key' => 'Primary Key',
+    'primary_key_not_found' => "Unable to find a primary key for :model, please specify a column with 'primary_key' => true or add a column with your model's primary key.",
+    /**
+     * just noting that the previous item wraps the line
+     */
     'remap_import' => 'Remap Import',
-    'your_import_has_been_queued' => 'Your import has been queued and will be processed in the background.',
+    'restart_import' => 'Restart Import',
+    'select_a_column' => 'Select a file column',
+    'select_a_file' => 'Select a file to import',
+    'start_import' => 'Start Import',
+    'start_over' => 'Start Over',
+    'upload_new_file' => 'Upload New File',
     'your_import_has_been_processed' => 'Your import has been processed successfully.',
-    'cant_find_log' => 'The specified import could not be found.',
-    'download_example' => 'Click here to download an example file.',
+    'your_import_has_been_queued' => 'Your import has been queued and will be processed in the background.',
+
+    /**
+     * The following describe column types in mappings.
+     */
     'text' => 'Text',
     'number' => 'Number',
     'date' => 'Date',
