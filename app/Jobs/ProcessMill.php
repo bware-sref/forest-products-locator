2266c9b9c1909edf013d12478c418485a0c6b3b9
Bob Ware <bware@sref.info>
2026-07-06 16:40:46 -0400

    add null safe to batch check comments

--- app/Jobs/ProcessMill.php
+++ app/Jobs/ProcessMill.php
@@ -25,9 +25,9 @@ class ProcessMill implements ShouldQueue
      */
     public function handle(): void
     {
-        if ($this->batch()->cancelled()) {
+        if ($this?->batch()?->cancelled()) {
             // The batch has been cancelled...
-            Log::debug(self::class.': Apparently this job batch was cancelled?');
+            Log::debug(self::class.': Apparently the job batch for Import #'.$this->mill->import_id.' or Mill #'.$this->mill->id.' was cancelled?');
 
             return;
         }
@@ -35,11 +35,19 @@ class ProcessMill implements ShouldQueue
         /**
          * what all do we need to do?
          *  - relationships
+         *      - address (a.k.a., state and county)
          *      - state_id should already be present
          *      - county_id
          *      - mailing_state_id
          *      - mill_types
          *      - wood_species
+         * 
+         * We need to be sure we have a usable address at this point.
+         */
+
+        /**
+         * Do we really even need this job since we now have individual jobs for MillTypes and WoodSpecies?
+         * Yes, we probably still need this job (or equivalent) because we haven't yet created the relationships for mailing_state_id and county_id.
          */
 
     }
