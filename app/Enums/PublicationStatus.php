1a8957c4aa9f825fc94963392d2f820e95511de8
Bob Ware <bware@sref.info>
2026-07-06 14:35:42 -0400

    add Error status

--- app/Enums/PublicationStatus.php
+++ app/Enums/PublicationStatus.php
@@ -11,8 +11,17 @@ enum PublicationStatus: string
     case Pending = 'pending';
     case Rejected = 'rejected';
     /**
-     * Invalid is for items that encounter issues during post-import processing.
-     * For example, if a mill is imported but has neither an address nor lat&lng with which to geocode it, it would be marked invalid.
+     * Invalid is for Mills that encounter issues that are not errors (but which need to be differentiated somehow) 
+     * during post-import processing.
+     * For example, if after geocoding, a mill still doesn't have an addresss (or, more importantly, lat&lng) it would be 
+     * considered invalid.
      */
     case Invalid = 'invalid';
+
+    /**
+     * Error is used for Mills that encounter errors during post-import processing
+     * For example, if a mill is imported but has neither an address nor lat&lng with which to geocode it, it would be marked invalid.
+     * If an error occurred preventing us from completing the import for an item, then it would be marked error.
+     */
+    case Error = 'error';
 }
