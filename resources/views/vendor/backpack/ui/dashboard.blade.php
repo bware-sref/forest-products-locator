f4baf33843fccac99135f5dd6745f36ec7178cb9
Bob Ware <bware@sref.info>
2026-08-04 15:48:35 -0400

    clear the crud off the admin dashboard

--- resources/views/vendor/backpack/ui/dashboard.blade.php
+++ resources/views/vendor/backpack/ui/dashboard.blade.php
@@ -3,28 +3,28 @@
 @php
     if (backpack_theme_config('show_getting_started')) {
         $widgets['before_content'][] = [
-            'type'        => 'view',
-            'view'        => backpack_view('inc.getting_started'),
+            'type' => 'view',
+            'view' => backpack_view('inc.getting_started'),
         ];
     } else {
         $widgets['before_content'][] = [
-            'type'        => 'jumbotron',
+            'type' => 'jumbotron',
             // trans() is the translate method
             // backpack::base.welcome corresponds to the 'welcome' array key in /vendor/backpack/crud/src/resources/lang/en/base.php
-            'heading'     => trans('backpack::base.welcome'),
-            'heading_class' => 'display-3 '.(backpack_theme_config('layout') === 'horizontal_overlap' ? ' text-white' : ''),
-            'content'     => 'What sidebar?', //trans('backpack::base.use_sidebar'),
+            'heading' => trans('backpack::base.welcome'),
+            'heading_class' => 'display-3 ' . (backpack_theme_config('layout') === 'horizontal_overlap' ? ' text-white' : ''),
+            'content' => 'To Fantasy Isle!', //trans('backpack::base.use_sidebar'),
             'content_class' => backpack_theme_config('layout') === 'horizontal_overlap' ? 'text-white' : '',
             'button_link' => backpack_url('logout'),
             'button_text' => trans('backpack::base.logout'),
         ];
 
-        $widgets['before_content'][] = [
-            'type' => 'chip',
-            'view' => 'crud::chips.general',
-            'title' => 'Chip!',
-            'entry' => 'huh?',
-        ];
+        // $widgets['before_content'][] = [
+        //     'type' => 'chip',
+        //     'view' => 'crud::chips.general',
+        //     'title' => 'Chip!',
+        //     'entry' => 'huh?',
+        // ];
 
 
 
@@ -40,20 +40,20 @@
 
         ];
 
-        foreach ($bgs as $bg) {
-            $widgets['before_content'][] = [
-                'type' => 'card',
-                'content' => [
-                    'header' => 'I am a '.$bg.' card header!',
-                    'body' => 'I am a '.$bg.' card body!',
-                ],
-                'wrapper' => [
-                    'class' => 'com-sm-6 col-md-4',
-                    'style' => '', // 'border-radius: 10px;',
-                ],
-                'class' => 'card bg-' . $bg,
-            ];
-        }
+        // foreach ($bgs as $bg) {
+        //     $widgets['before_content'][] = [
+        //         'type' => 'card',
+        //         'content' => [
+        //             'header' => 'I am a '.$bg.' card header!',
+        //             'body' => 'I am a '.$bg.' card body!',
+        //         ],
+        //         'wrapper' => [
+        //             'class' => 'com-sm-6 col-md-4',
+        //             'style' => '', // 'border-radius: 10px;',
+        //         ],
+        //         'class' => 'card bg-' . $bg,
+        //     ];
+        // }
 
     }
 @endphp
