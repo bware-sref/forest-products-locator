2b25e3a9f9c38de1b3a05253ccb2ed212f8a9171
Bob Ware <bware@sref.info>
2026-08-06 14:14:16 -0400

    comments

--- app/Http/Controllers/Admin/StatePageCrudController.php
+++ app/Http/Controllers/Admin/StatePageCrudController.php
@@ -58,14 +58,6 @@ class StatePageCrudController extends CrudController
             ->model('App\Models\State')
             ->attribute('name')
             ->orderable(true);
-            /**
-             * order logic doesn't work like we want
-             */
-            // ->orderLogic(function ($query, $column, $columnDirection) {
-            //     return $query->leftJoin('states', 'states.id', '=', 'state_pages.state_id')
-            //         ->orderBy('states.name', $columnDirection)
-            //         ->select('state_pages.*'); // prevent column name collisions
-            // });
         CRUD::column('hero_headline')
             ->type('text')
             ->orderable(true);
