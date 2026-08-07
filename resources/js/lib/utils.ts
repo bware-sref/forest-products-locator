31c564354c5c8fb13fef04a56b40918a2a5c9018
Bob Ware <bware@sref.info>
2026-08-07 09:09:16 -0400

    add IsExternalUrl() method to utils.ts

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -23,3 +23,23 @@ export function isChildUrl(
 ) {
     return !isSameUrl(childUrl, parentUrl) && resolveUrl(childUrl).startsWith(resolveUrl(parentUrl));
 }
+
+export function isExternalUrl(
+    url: NonNullable<InertiaLinkProps['href']>,
+    siteUrl: NonNullable<InertiaLinkProps['href']>,
+) {
+    console.log('unresolved url: ', url);
+    const rUrl = resolveUrl(url);
+    let parsedUrl;
+    // We can identify relative links by trying to instantiate a new URL() with the URL we want to test.
+    // Incomplete or malformed URLs cause a TypeError exception.
+    try {
+        parsedUrl = new URL(rUrl);
+        // if parsing the url causes an exception, it's not an absolute URL
+    } catch (e) {
+        console.error(`exception when parsing URL: '${rUrl}': `, e);
+        return false;
+    }
+    const parsedSiteUrl = new URL(resolveUrl(siteUrl));
+    return parsedUrl.hostname !== parsedSiteUrl.hostname;
+}
