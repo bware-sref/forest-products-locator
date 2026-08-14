01a91a3c7c70c8eadf16bf1e3e84651091288315
Bob Ware <bware@sref.info>
2026-08-14 10:18:08 -0400

    add bias_position to geocoding configs

--- config/geocoding.php
+++ config/geocoding.php
@@ -23,6 +23,39 @@ return [
     'rate_limits' => [
         'per_minute' => env('GEOCODING_RATE_LIMIT_PER_MINUTE', 10),
         'per_day' => env('GEOCODING_RATE_LIMIT_PER_DAY', 200),
-    ],
+    ],    
 
+    /**
+     * Bias Position
+     * 
+     * Bias Position is the position that location search results should be close to.
+     * This should reduce the sort of weirdness I encountered when searching "120 lakeshore" and the results were in Canada.
+     * 
+     * OMG, I gotta go doody.
+     * 
+     * Anyway, when included in an Amazon Location Service API request, it's formatted as an array ordered as [X,Y],
+     * meaning [longitude, latitude] rather than how they're typical ordered in layperson speech.
+     * X, Y is apparently old hat for the geospatial folks, not just because they're spatial, but because they're geospatial. :-D
+     * Our default coordinates were determined by finding the rough bounding box for the southeastern states and then finding the
+     * center.
+     * Deets below
+     * Kill Devil Hills, NC (outer banks) is just shy of 75° W (-75.71) so -75.
+     * El Paso, TX is just over 106° W (-106.58) so -107.
+     * The north western corner of Oklahoma is about 36.99° N, but the northern tip of Virginia is at 39.36° N, so 40.
+     * The southern tip of Texas is about 25.86° N but Key West is only about 24.57° N, so 24.
+     * Remember, it doesn't need to insanely precise, just enough to minimize results that are way off.
+     * 
+     * ((-107 - -75) / 2) + -75 = -91.0
+     * ((40 - 24) / 2) + 24 = 32.0
+     * I can't remember if adding .0 actually makes PHP treat it as a float or not.
+     * 
+     * Also, should this be our default map center?
+     * 
+     * I waffled between using long names versus x & y.
+     * Look what won!
+     */
+    'bias_position' => [
+        'x' => env('GEOCODING_BIAS_POSITION_X', -91.0),
+        'y' => env('GEOCODING_BIAS_POSITION_Y', 32.0),
+    ],
 ];
