ab3752e7981ee23c2bf65ce2cac640c470b739d6
Bob Ware <bware@sref.info>
2026-08-14 11:14:54 -0400

    fix dashboard test to work with backpack

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -3,11 +3,22 @@
 use App\Models\User;
 
 test('guests are redirected to the login page', function () {
-    $this->get(route('dashboard'))->assertRedirect(route('login'));
+    $this->get(route('backpack.dashboard'))->assertRedirect(route('backpack.auth.login'));
 });
 
 test('authenticated users can visit the dashboard', function () {
-    $this->actingAs($user = User::factory()->create());
+    /**
+     * actingAs() is spozta be the fake login.
+     * However, since we might be using Backpack's auth guards, we need to specify that when invoking actingAs().
+     * Also, it might still break if we don't assign some roles and/or permissions to the new User.
+     */
+    $user = User::factory()->create();
 
-    $this->get(route('dashboard'))->assertOk();
+    $backpackGuard = config('backpack.base.guard', 'backpack');
+
+    $this->actingAs($user, $backpackGuard);
+    /**
+     * The following fails because creating the user does not automatically authenticate them.
+     */
+    $this->get(route('backpack.dashboard'))->assertOk();
 });
