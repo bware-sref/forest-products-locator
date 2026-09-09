78a3307187a3570052fb31b9d468632ea3f018e7
Bob Ware <bware@sref.info>
2026-09-09 16:37:44 -0400

    complete nonsense

--- app/Http/Controllers/MillEditController.php
+++ app/Http/Controllers/MillEditController.php
@@ -2,65 +2,53 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreMillEditRequest;
-use App\Http\Requests\UpdateMillEditRequest;
+// use App\Http\Requests\StoreMillEditRequest;
+// use App\Http\Requests\UpdateMillEditRequest;
 use App\Models\MillEdit;
+use Illuminate\Support\Facades\Log;
+use Inertia\Inertia;
+
 
 class MillEditController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * How do we check and/or inject the hash?
+     * @return void
      */
-    public function index()
+    public function approve()
+    {}
+
+    public function reject()
     {
-        //
+
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Actually, I dont' think we need show().
+     * Instead, approve and reject should show the differences and include an "Are you sure?" button...
+     * Actually, taking that approach, we don't even need separate hashes for approve and reject.
+     * Instead, show() shows the differences along with approve and reject buttons.
+     * @param MillEdit $millEdit
+     * @return \Inertia\Response
      */
-    public function create()
+    public function show(MillEdit $millEdit)
     {
-        //
-    }
+        Log::debug("MillEditC::show(): WTF? ", [
+            'millEdit' => $millEdit->toArray(),
+            'mill' => $millEdit->mill->toArray(),
+        ]);
+        
+        $mill = $millEdit->mill;
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreMillEditRequest $request)
-    {
-        //
-    }
+        $submission = $mill->replicate();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(MillEdit $millEdits)
-    {
-        //
-    }
+        $submission->fill(json_decode($millEdit->proposed_changes, true));
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(MillEdit $millEdits)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateMillEditRequest $request, MillEdit $millEdits)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(MillEdit $millEdits)
-    {
-        //
+        return Inertia::render('mill-edit-show', [
+            'original' => $mill->original,
+            'submission' => $submission,
+            'millEdit' => $millEdit,
+            'mill' => $mill,
+        ]);
     }
 }
