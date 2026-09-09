00598b9d03d5aa4b8eb01e709edea8c4e23b8287
Bob Ware <bware@sref.info>
2026-09-09 16:37:15 -0400

    add booted methods to fill required fields

--- app/Models/MillEdit.php
+++ app/Models/MillEdit.php
@@ -9,6 +9,8 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\URL;
 
 /**
  * @mixin IdeHelperMillEdit
@@ -24,6 +26,7 @@ class MillEdit extends Model
         // there's an argument for adding a state_id column to these, even though it can be derived from mill_id
         'submitter_email',
         'submitter_ip',
+        'url',
         'approve_hash',
         'reject_hash',
         'proposed_changes',
@@ -36,6 +39,38 @@ class MillEdit extends Model
     protected $casts = [
         'status' => PublicationStatus::class,
     ];
+
+    protected static function booted(): void
+    {
+        static::creating(function (MillEdit $me) {
+            if (empty($me->approve_hash)) {
+                // $me->approve_hash = Hash::make("approve:{$me->proposed_changes}");
+                // URL::temporarySignedRoute(
+                //     'mill-edits.show',
+                //     now()->addDays(30),
+                //     ['mill_edit' => $me->id],
+                // );
+            }
+
+            if (empty($me->reject_hash)) {
+                // $me->reject_hash = Hash::make("reject:{$me->proposed_changes}");
+                // URL::temporarySignedRoute(
+                //     'mill-edits.show',
+                //     now()->addDays(30),
+                //     ['mill_edit' => $me->id],
+                // );
+            }
+
+            if (empty($me->url)) {
+                $me->url = URL::temporarySignedRoute(
+                    'mill-edits.show',
+                    now()->addDays(90),
+                    ['mill_edit' => $me->id]
+                );
+            }
+        });
+    }
+
 
     /**
      * The glue.
@@ -66,5 +101,14 @@ class MillEdit extends Model
     protected function rejected(Builder $query): void
     {
         $query->where('status', PublicationStatus::Rejected);
+    }
+
+    protected function pyttIPanna(): string
+    {
+        return URL::temporarySignedRoute(
+            'mill-edits.show',
+            now()->addDays(30),
+            ['mill_edit' => $this->id],
+        );
     }
 }
