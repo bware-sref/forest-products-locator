88f7ff42ab9fee0953ebc01eb25c4ddcd598dcf5
Bob Ware <bware@sref.info>
2026-09-09 14:02:02 -0400

    hoist common things into AbstractMillRequest

--- app/Http/Requests/UpdateMillRequest.php
+++ app/Http/Requests/UpdateMillRequest.php
@@ -2,28 +2,30 @@
 
 namespace App\Http\Requests;
 
-/**
- * Unless there end up being differences between this and StoreMillRequest, perhaps we should just use it.
- */
-class UpdateMillRequest extends StoreMillRequest
+class UpdateMillRequest extends AbstractMillRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
-    // public function authorize(): bool
-    // {
-    //     return true;
-    // }
+    public function authorize(): bool
+    {
+        return true;
+    }
 
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
-    // public function rules(): array
-    // {
-    //     return [
-    //         //
-    //     ];
-    // }
+    public function rules(): array
+    {
+        return parent::baseRules();
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->mergeSubmitterIp();
+        $this->mergeStatus();
+        $this->handleMailingAddress();
+    }    
 }
