f35a51d801cc69379bea7f9f9a166b14ca5515d8
Bob Ware <bware@sref.info>
2026-09-10 16:01:56 -0400

    prevent indent on save that was breaking markdown, FFS!!!

--- resources/views/mail/mill_edit_notification.blade.php
+++ resources/views/mail/mill_edit_notification.blade.php
@@ -1,17 +1,18 @@
 <x-mail::message>
-    # New Mill Update submitted
 
-    **Mill Name:**
-    {{ $mill_name }}
+# New Mill Update submitted
 
-    <x-mail::button :url="'#'">
-        Approve
-    </x-mail::button>
+**Mill Name:**<br />
+{{ $mill_name }}
 
-    <x-mail::button :url="''">
-        Reject
-    </x-mail::button>
+<x-mail::button :url="'#'">
+    Approve
+</x-mail::button>
 
-    Thanks,<br>
-    {{ config('app.name') }}
+<x-mail::button :url="''">
+    Reject
+</x-mail::button>
+
+Thanks,<br>
+{{ config('app.name') }}
 </x-mail::message>
