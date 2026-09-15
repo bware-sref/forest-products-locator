7a1c08c691f1d362209f06265ff216194e3dd987
Bob Ware <bware@sref.info>
2026-09-15 16:12:49 -0400

    flesh out approve and reject() add pendingOrDie() to check for valid hashes

--- app/Http/Controllers/MillEditController.php
+++ app/Http/Controllers/MillEditController.php
@@ -2,27 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\PublicationStatus;
 use App\Mail\MillEditNotification;
 use App\Models\Mill;
 use App\Models\MillEdit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
+use Throwable;
 
 class MillEditController extends Controller
 {
     /**
      * How do we check and/or inject the hash?
-     * @return void
      */
     public function approve(MillEdit $millEdit)
     {
+        self::pendingOrDie($millEdit);
 
+        /**
+         * I think it might be as simple as using fill(), then looping over the N_to_N relations.
+         * In any case, that stuff should go in a model method.
+         * I suppose we should wrap this in try{}
+         */
+
+        try {
+            $millEdit->approve();
+        } catch (Throwable $e) {
+            $msg = "Failed to process approval of MillEdit #{$millEdit->id}!";
+            Log::error("\n".self::class."::approve():\n{$msg}", [
+                "\nerror\n" => $e->getMessage(),
+                "\nmillEdit\n" => $millEdit->toArray(),
+            ]);
+            Inertia::flash([
+                'type' => 'error',
+                'message' => $msg,
+            ]);
+            return to_route('mill-edits.show', $millEdit);
+        }
+
+        $msg = "Approved MillEdit #{$millEdit->id} for Mill #{$millEdit->mill->id}.";
+        Log::debug("\n".self::class."::approve():\n{$msg}");
+        Inertia::flash([
+            'type' => 'success',
+            'message' => $msg,
+        ]);
+
+        return to_route('mills.show', $millEdit->mill);
     }
 
     public function reject(MillEdit $millEdit)
     {
+        self::pendingOrDie($millEdit);
 
+        if (! $millEdit->reject()) {
+            /**
+             * What to do if it fails?
+             */
+            $msg = "Failed to reject MillEdit #{$millEdit->id}";
+            Log::error("\n".self::class."::rejected():\n{$msg}");
+            Inertia::flash([
+                'type' => 'error',
+                'message' => $msg,
+            ]);
+            return to_route('mill-edits.show', $millEdit);
+        }
+
+        $msg = "Rejected MillEdit #{$millEdit->id}.";
+        Log::debug("\n".self::class."::rejected():\n{$msg}\n");
+        Inertia::flash([
+            'type' => 'success',
+            'message' => $msg,
+        ]);
+        return to_route('mills.show', $millEdit->mill);
     }
 
     /**
@@ -37,26 +89,13 @@ class MillEditController extends Controller
      */
     public function show(Request $request, MillEdit $millEdit)
     {
-        /**
-         * signature doesn't work exactly as I'd hoped.
-         * It can be checked and verified, but it can't be expired early.
-         */
-        if (! $request->hasValidSignature()) {
-            Log::error(self::class."::show(): invalid signature!");
-            /**
-             * Where to redirect to?
-             * 404'd!
-             *
-             * Or, we could redirect to the Mill that was edited...
-             */
-            abort(404);
-        }
+        self::pendingOrDie($millEdit);
         
         /**
          * We could/should probably extract the mess below into a MillEdit model method.
          */
-        $original = $millEdit->originalMill('name', except: MillEdit::OMIT_FROM_DIFF_DISPLAY);
-        $submitted = $millEdit->prepareSubmitted('name', except: MillEdit::OMIT_FROM_DIFF_DISPLAY);
+        $original = $millEdit->originalMill();
+        $submitted = $millEdit->prepareSubmitted();
         $changes = $millEdit->getChanges();
         $diff = $millEdit->getDiff();
 
@@ -81,11 +120,11 @@ class MillEditController extends Controller
             'original' => $original,
             'submitted' => $submitted,
             'millEdit' => $millEdit,
+            // I don't think we need changes or diff in the view
             'changes' => $changes,
             'diff' => $diff,
         ]);
     }
-
 
     /**
      * Preview a MillEdit notification email.
@@ -99,4 +138,23 @@ class MillEditController extends Controller
         return new MillEditNotification($millEdit);
     }
 
+    /**
+     * Should we just pass the status?
+     * @param MillEdit $millEdit
+     * @return void
+     */
+    protected static function pendingOrDie(MillEdit $millEdit)
+    {
+        if ($millEdit->status === PublicationStatus::Pending) {
+            return;
+        }
+        Log::error("\n".self::class."::pendingOrDie():\nattempt to approve, reject, or review MillEdits which are no longer pending?!?\n", [
+            'millEdit' => $millEdit->toArray(),
+        ]);
+        /**
+         * Where to redirect to?
+         * 404'd!
+         */
+        abort(404);
+    }
 }
